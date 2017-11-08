1293fa24214f134194e7660c95e874beed363073
goodbye <verstraete.nina@gmail.com>
2017-11-08 16:38:02 +0100

    Add files via upload

---
+++ test/hero_detail_po.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+
+import 'package:pageloader/objects.dart';
+import 'utils.dart';
+
+class HeroDetailPO {
+  @FirstByCss('div h2')
+  PageLoaderElement _title; // e.g. 'Mr Freeze details!'
+
+  @FirstByCss('div div')
+  PageLoaderElement _id;
+
+  @ByTagName('input')
+  PageLoaderElement _input;
+
+  @ByTagName('button')
+  PageLoaderElement _button;
+
+  Future<Map> get heroFromDetails async {
+    if (_id == null) return null;
+    final idAsString = (await _id.visibleText).split(':')[1];
+    final text = await _title.visibleText;
+    final matches = new RegExp((r'^(.*) details!$')).firstMatch(text);
+    return heroData(idAsString, matches[1]);
+  }
+
+  Future clear() => _input.clear();
+  Future type(String s) => _input.type(s);
+
+  Future back() => _button.click();
+}
