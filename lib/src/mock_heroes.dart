af88d6bc8d1e3bd3b760cceaa2f776eacc669540
goodbye <verstraete.nina@gmail.com>
2017-11-08 16:35:34 +0100

    Create mock_heroes.dart

---
+++ lib/src/mock_heroes.dart
@@ -0,0 +1,14 @@
+import 'hero.dart';
+
+final mockHeroes = <Hero>[
+  new Hero(11, 'Mr. Nice', 0, 0, 1000, 100, 100, ['Offense']),
+  new Hero(12, 'Narco', 0, 0, 1000, 100, 100, ['Offense']),
+  new Hero(13, 'Bombasto', 0, 0, 1000, 100, 100, ['Offense']),
+  new Hero(14, 'Celeritas', 0, 0, 1000, 100, 100, ['Offense']),
+  new Hero(15, 'Magneta', 0, 0, 1000, 100, 100, ['Offense']),
+  new Hero(16, 'RubberMan', 0, 0, 1000, 100, 100, ['Offense']),
+  new Hero(17, 'Dynama', 0, 0, 1000, 100, 100, ['Offense']),
+  new Hero(18, 'Dr IQ', 0, 0, 1000, 100, 100, ['Offense']),
+  new Hero(19, 'Magma', 0, 0, 1000, 100, 100, ['Offense']),
+  new Hero(20, 'Tornado', 0, 0, 1000, 100, 100, ['Offense'])
+];
